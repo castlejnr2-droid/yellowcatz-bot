eba3e615bb35724447b8c9c01f81e544b893e21e
castlejnr2-droid <castlejnr2@gmail.com>
2026-04-08 04:39:13 +0100

    Collect amount updated

--- src/bot/commands/collect.js
+++ src/bot/commands/collect.js
@@ -1,8 +1,8 @@
 const db = require('../../db/queries');
 
 const COOLDOWN_MS = 5 * 60 * 1000; // 5 minutes
-const MIN_COLLECT = 15;
-const MAX_COLLECT = 50;
+const MIN_COLLECT = 5;
+const MAX_COLLECT = 40;
 const REFERRAL_BONUS = 50;
 
 function randomCollectAmount() {
